b47d7debd9d9ce1353e7b3fb46fd09ca706c1b46
cosre7 <cosre7@gmail.com>
2021-03-16 17:27:29 +0900

    29-c-client

--- myproject-01-client/app/src/main/java/com/cosre7/pms/ClientApp.java
+++ myproject-01-client/app/src/main/java/com/cosre7/pms/ClientApp.java
@@ -3,6 +3,7 @@ package com.cosre7.pms;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import com.cosre7.pms.util.Prompt;
 
 public class ClientApp {
 
@@ -25,11 +26,21 @@ public class ClientApp {
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
         DataInputStream in = new DataInputStream(socket.getInputStream())) {
 
-      out.writeUTF("hello");
-      out.flush();
+      while (true) {
+        String message = Prompt.inputString("명령> ");
 
-      String response = in.readUTF();
-      System.out.println(response);
+        out.writeUTF(message);
+        out.flush();
+
+        String response = in.readUTF();
+        System.out.println(response);
+
+        if (message.equals("quit")) {
+          break;
+        }
+      }
+
+      Prompt.close();
 
     } catch (Exception e) {
       System.out.println("서버와 통신 하는 중에 오류 발생!");
---
+++ myproject-01-client/app/src/main/java/com/cosre7/pms/util/Prompt.java
@@ -0,0 +1,29 @@
+package com.cosre7.pms.util;
+
+import java.sql.Date;
+import java.util.Scanner;
+
+public class Prompt {
+  public static Scanner keyboardScan = new Scanner(System.in);
+
+  public static String inputString (String title) {
+    System.out.print(title);
+    return keyboardScan.nextLine();
+  }
+
+  public static Date inputDate (String title) {
+    return Date.valueOf(inputString(title));    
+  }
+
+  public static int inputInt (String title) {
+    return Integer.valueOf(inputString(title));
+  }
+
+  public static double inputDouble (String title) {
+    return Double.parseDouble(inputString(title));
+  }
+
+  public static void close() {
+    keyboardScan.close();
+  }
+}
